37784cd7963750ec4edaed1e135bee17cf3612a5
ik4y <imadk9700@gmail.com>
2024-09-14 02:13:00 +0500

    removed tailwind added TS

--- client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="w-100 h-auto bg-red-500">
-      <h1 className="text-3xl text-red-950 font-bold underline">Hello world</h1>
-    </div>
-  );
-}
